19cad932e7c7383ed728d9711e360b4ef4835b00
kozzzx <kozzzx@qq.com>
2017-06-02 17:51:35 +0800

    可以给get_json传入额外的参数，并一起给予验证器验证

--- flask_apikit/views.py
+++ flask_apikit/views.py
@@ -9,12 +9,13 @@ from flask_apikit.exceptions import ValidateError
 class ApiView(MethodView):
     decorators = (api_view,)
 
-    def get_json(self, schema=None, *args, **kwargs):
+    def get_json(self, schema=None, extra_data=None, *args, **kwargs):
         """
         从request获取json数据,没有则返回空字典
         可以使用一个验证器进行数据验证
 
         :param Schema schema: 验证器
+        :param dict extra_data: 额外的数据，会覆盖request的内容，并一起给予验证器验证
         :param args:
         :param kwargs:
         :rtype: dict
@@ -25,6 +26,9 @@ class ApiView(MethodView):
         # json_data为None,转为空字典
         if json_data is None:
             json_data = {}
+        # 如果有额外的数据，合并到字典中
+        if extra_data:
+            json_data = dict(json_data, **extra_data)
         # 给了验证器,则进行验证
         if isinstance(schema, Schema):
             data, errors = schema.load(json_data)
