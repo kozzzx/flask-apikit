cb93c98be0584d42ca219902bf299a5cacd3c4a2
kozzzx <kozzzx@qq.com>
2017-07-20 16:22:00 +0800

    修改ApiKit类

--- flask_apikit/__init__.py
+++ flask_apikit/__init__.py
@@ -1,7 +1,8 @@
 class ApiKit:
     def __init__(self, app=None):
+        self.app = app
         if app is not None:
             self.init_app(app)
 
-    def init_app(self):
+    def init_app(self, app):
         pass
