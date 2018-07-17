2afc014a8df6bff15e084cb607ca67e6b20af926
kozzzx <kozzzx@qq.com>
2018-07-17 11:01:52 +0800

    edit setup.py

--- setup.py
+++ setup.py
@@ -5,21 +5,22 @@ Flask-APIKit
 
 Build Restful API with Flask Quickly.
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='Flask-APIKit',
-    version='0.0.2',
+    version='0.0.4',
     url='https://github.com/kozzzx/flask-apikit',
     license='BSD',
     author='kozzzx',
     author_email='kozzzx@qq.com',
     description='Build Restful API with Flask Quickly.',
     long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'Flask', 'marshmallow'
+        'Flask>=1.0', 'marshmallow==3.0.0b8'
     ],
     classifiers=[
         'Environment :: Web Environment',
